88e416fcf899c04763d956c4ed7c7125dfcf8c23
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-20 11:32:50 -0700

    Add cheetah section and js to dissapear.
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   $(".clickable-walrus").click(function() {
     $(".walrus-showing").fadeToggle();
     $(".walrus-hidden").fadeToggle();
+  });
+  $(".clickable-giraffe").click(function() {
     $(".wiki-page").slideToggle();
+  });
+  $(".click-cheetah").click(function() {
+    $(".cheetah-hide").slideToggle();
   });
 });
